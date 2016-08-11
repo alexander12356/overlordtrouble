9dc1026479139c379b3aa650c6b7951de67b5820
missingup <l1sten51091@gmail.com>
2016-08-11 16:14:04 +0900

    переделал энциклопедию, согласно тех документу. получило одобрение вовы

--- Assets/Codes/Encyclopedia/MainMonsterInfo.cs
+++ Assets/Codes/Encyclopedia/MainMonsterInfo.cs
@@ -3,10 +3,7 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class MainMonsterInfo : MonoBehaviour {
-	public Text MonsterName = null;
-	public Text MonsterID = null;
 	public Text MonsterDescription = null;
-	public Text MonsterAttack = null;
-	public Text MonsterHp = null;
-	public Text MonsterDefence = null;
+	public Text MonsterElement = null;
+
 }
