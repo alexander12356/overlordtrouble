a44a521182e246ccbb7031eca652377049a67c86
neuro256 <neuro256@mail.ru>
2016-09-13 15:14:28 +0900

    Реализовать переходы внутрь дома

---
+++ Assets/Codes/JourneySystemClasses/Warp.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Warp : MonoBehaviour {
+
+    [SerializeField]
+    private Transform warpTarget = null;
+
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        Debug.Log("An object collided");
+        otherCollider.gameObject.transform.parent.transform.position = warpTarget.position;
+        // Camera.main.transform.position = warpTarget.position;
+    }
+}
