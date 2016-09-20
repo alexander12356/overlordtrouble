0ade141e0d4afa87106fcb594435c8f81e0878c0
neuro256 <neuro256@mail.ru>
2016-09-20 11:04:01 +0900

    Камера не должна пойти дальше края земли

--- Assets/Codes/CameraFollow.cs
+++ Assets/Codes/CameraFollow.cs
@@ -9,12 +9,17 @@ public class CameraFollow : MonoBehaviour
 
     [SerializeField]
     private Transform m_Target = null;
+    [SerializeField]
+    private BoxCollider2D Bounds;
+    private Vector3 borderMin, borderMax;
 	
 	public void Start()
     {
         m_Camera = GetComponent<Camera>();
         m_Transform = transform;
         m_Delta = new Vector3(0.0f, 0.0f, -10.0f);
+        borderMin = Bounds.bounds.min;
+        borderMax = Bounds.bounds.max;
     }
 	
 	public void Update()
@@ -24,6 +29,9 @@ public class CameraFollow : MonoBehaviour
         if (m_Target != null)
         {
             m_Transform.position = Vector3.Lerp(m_Transform.position, m_Target.position, 0.1f) + m_Delta;
+            var cameraHalfWidth = GetComponent<Camera>().orthographicSize * ((float)Screen.width / Screen.height);
+            m_Transform.position = new Vector3(Mathf.Clamp(m_Transform.position.x, borderMin.x + cameraHalfWidth, borderMax.x - cameraHalfWidth), 
+                Mathf.Clamp(m_Transform.position.y, borderMin.y + GetComponent<Camera>().orthographicSize, borderMax.y - GetComponent<Camera>().orthographicSize), m_Transform.position.z);
         }
 	}
 }
