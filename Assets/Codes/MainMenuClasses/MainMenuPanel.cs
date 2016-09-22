82f853a946b62edf0c5461ba3f0ef365b214c740
Alexander12356 <alexander12356@rambler.ru>
2016-09-22 18:35:14 +0900

    Добавлен главное мегю игры. Переход в тестовый режим чрез кнопку F12

---
+++ Assets/Codes/MainMenuClasses/MainMenuPanel.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuPanel : Panel
+{
+    private ButtonList m_ButtonList = null;
+    private PanelManager m_PanelManager = null;
+
+    public override void Awake()
+    {
+        base.Awake();
+
+        m_PanelManager = GetComponentInParent<PanelManager>();
+
+        m_ButtonList = GetComponentInChildren<ButtonList>();
+        m_ButtonList[0].AddAction(RunNewGame);
+        m_ButtonList[1].AddAction(QuitGame);
+    }
+
+    public void Update()
+    {
+        UpdatePanel();
+    }
+
+    public override void UpdatePanel()
+    {
+        base.UpdatePanel();
+
+        m_ButtonList.UpdateKey();
+
+        if (Input.GetKeyUp(KeyCode.F12))
+        {
+            m_PanelManager.StartLocation("DemoMainScene");
+        }
+    }
+
+    private void RunNewGame()
+    {
+        m_PanelManager.StartLocation("Town");
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
+    }
+}
