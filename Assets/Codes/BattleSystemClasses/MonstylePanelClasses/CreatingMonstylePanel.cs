4b44629d196301561fdddbd81c931e7cafc55326
Alexander12356 <alexander12356@rambler.ru>
2016-12-21 15:11:51 +0900

    Добавлены новый спешл: Удар тростью и Искра

--- Assets/Codes/BattleSystemClasses/MonstylePanelClasses/CreatingMonstylePanel.cs
+++ Assets/Codes/BattleSystemClasses/MonstylePanelClasses/CreatingMonstylePanel.cs
@@ -242,6 +242,20 @@ public class CreatingMonstylePanel : Panel
             l_MonstyleButton.AddAction(ChooseSpecial);
             m_SpecialButtonList.AddButton(l_MonstyleButton);
         }
+        for (int i = 0; i < 1; i++)
+        {
+            PanelButtonSpecial l_MonstyleButton = Instantiate(PanelButtonSpecial.prefab);
+            l_MonstyleButton.specialId = "StaffStrike";
+            l_MonstyleButton.AddAction(ChooseSpecial);
+            m_SpecialButtonList.AddButton(l_MonstyleButton);
+        }
+        for (int i = 0; i < 1; i++)
+        {
+            PanelButtonSpecial l_MonstyleButton = Instantiate(PanelButtonSpecial.prefab);
+            l_MonstyleButton.specialId = "Ember";
+            l_MonstyleButton.AddAction(ChooseSpecial);
+            m_SpecialButtonList.AddButton(l_MonstyleButton);
+        }
     }
 
     private void InitSpecials()
