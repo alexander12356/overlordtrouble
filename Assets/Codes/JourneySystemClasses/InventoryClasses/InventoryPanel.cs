2f50cc04835dd5cd160ddb5b42aa299bf28c05d7
neuro256 <neuro256@mail.ru>
2017-01-06 23:42:36 +0900

    Обновлены ресурсы инвентаря, обновлены кнопки нижней панели

--- Assets/Codes/JourneySystemClasses/InventoryClasses/InventoryPanel.cs
+++ Assets/Codes/JourneySystemClasses/InventoryClasses/InventoryPanel.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 public class InventoryPanel : Panel
 {
     private static InventoryPanel m_Prefab;
-    private InventoryTab m_CurrOpenedTab = null;
+    //private InventoryTab m_CurrOpenedTab = null;
     [SerializeField]
     private ButtonList m_TabButtonsList = null;
-    [SerializeField]
-    private List<InventoryTab> m_InventoryTabs = null;
+    //[SerializeField]
+    //private List<InventoryTab> m_InventoryTabs = null;
 
     public static InventoryPanel prefab
     {
@@ -45,38 +45,38 @@ public class InventoryPanel : Panel
         base.UpdatePanel();
 
         m_TabButtonsList.UpdateKey();
-        m_CurrOpenedTab.UpdateKey();
+        //m_CurrOpenedTab.UpdateKey();
     }
 
     private void InitTabs()
     {
-        m_TabButtonsList.AddKeyArrowAction(ShowTab);
-        m_TabButtonsList[0].AddAction(ConfirmTab);
-        m_TabButtonsList[0].title = LocalizationDataBase.GetInstance().GetText("GUI:Journey:Inventory:Wear");
-        m_TabButtonsList[1].AddAction(ConfirmTab);
-        m_TabButtonsList[1].title = LocalizationDataBase.GetInstance().GetText("GUI:Journey:Inventory:Items");
-        m_TabButtonsList[2].AddAction(CloseInventory);
-        m_TabButtonsList[2].title = LocalizationDataBase.GetInstance().GetText("GUI:Journey:Inventory:Back");
+        //m_TabButtonsList.AddKeyArrowAction(ShowTab);
+        //m_TabButtonsList[0].AddAction(ConfirmTab);
+        //m_TabButtonsList[0].title = LocalizationDataBase.GetInstance().GetText("GUI:Journey:Inventory:Wear");
+        //m_TabButtonsList[1].AddAction(ConfirmTab);
+        //m_TabButtonsList[1].title = LocalizationDataBase.GetInstance().GetText("GUI:Journey:Inventory:Items");
+        //m_TabButtonsList[2].AddAction(CloseInventory);
+        //m_TabButtonsList[2].title = LocalizationDataBase.GetInstance().GetText("GUI:Journey:Inventory:Back");
 
-        m_CurrOpenedTab = m_InventoryTabs[0];
+        //m_CurrOpenedTab = m_InventoryTabs[0];
     }
 
-    private void ConfirmTab()
-    {
-        m_CurrOpenedTab.Confrim();
-        m_TabButtonsList.isActive = false;
-        m_TabButtonsList.currentButton.selected = true;
-    }
+    //private void ConfirmTab()
+    //{
+    //    m_CurrOpenedTab.Confrim();
+    //    m_TabButtonsList.isActive = false;
+    //    m_TabButtonsList.currentButton.selected = true;
+    //}
 
-    private void ShowTab()
-    {
-        if (m_TabButtonsList.currentButtonId < m_InventoryTabs.Count)
-        {
-            m_CurrOpenedTab.gameObject.SetActive(false);
-            m_InventoryTabs[m_TabButtonsList.currentButtonId].gameObject.SetActive(true);
-            m_CurrOpenedTab = m_InventoryTabs[m_TabButtonsList.currentButtonId];
-        }
-    }
+    //private void ShowTab()
+    //{
+    //    if (m_TabButtonsList.currentButtonId < m_InventoryTabs.Count)
+    //    {
+    //        m_CurrOpenedTab.gameObject.SetActive(false);
+    //        m_InventoryTabs[m_TabButtonsList.currentButtonId].gameObject.SetActive(true);
+    //        m_CurrOpenedTab = m_InventoryTabs[m_TabButtonsList.currentButtonId];
+    //    }
+    //}
 
     private void CloseInventory()
     {
---
+++ Assets/Codes/JourneySystemClasses/InventoryClasses/InventoryTabButton.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine.UI;
+
+public class InventoryTabButton : PanelButton {
+    protected new bool m_Selected;
+    protected Image m_BackgroundImage = null;
+    protected new Image m_SelectedImage = null;
+
+    public new bool selected
+    {
+        get
+        {
+            return m_Selected;
+        }
+        set
+        {
+            Select(value);
+        }
+    }
+
+    public Image backgroundImage
+    {
+        get
+        {
+            if(m_BackgroundImage == null)
+            {
+                m_BackgroundImage = transform.GetChild(0).gameObject.GetComponent<Image>();
+            }
+            return m_BackgroundImage;
+        }
+    }
+
+    public new Image selectedImage
+    {
+        get
+        {
+            if (m_SelectedImage == null)
+            {
+                m_SelectedImage = transform.GetChild(1).gameObject.GetComponent<Image>();
+            }
+            return m_SelectedImage;
+        }
+    }
+
+    public override void Awake()
+    {
+        m_BackgroundImage = backgroundImage;
+        m_SelectedImage = selectedImage;
+
+        backgroundImage.gameObject.SetActive(true);
+        selectedImage.gameObject.SetActive(false);
+    }
+
+    public override void Select(bool p_Value)
+    {
+        m_Selected = p_Value;
+        backgroundImage.gameObject.SetActive(!m_Selected);
+        selectedImage.gameObject.SetActive(m_Selected);
+    }
+}
