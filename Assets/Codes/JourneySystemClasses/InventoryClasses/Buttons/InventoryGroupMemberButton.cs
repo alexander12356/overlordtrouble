44a678ad8dfc46f64f170d1df034b88816869680
neuro256 <neuro256@mail.ru>
2017-01-13 14:06:25 +0900

    Реализовано отображение группы в инвентаре. Пока еще сыро, требует доработки. + удалены старые арты

---
+++ Assets/Codes/JourneySystemClasses/InventoryClasses/Buttons/InventoryGroupMemberButton.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryGroupMemberButton : PanelButton
+{
+    private static InventoryGroupMemberButton m_Prefab = null;
+    private Image m_PlayerImage = null;
+    private Text m_HealthText = null;
+    private Text m_SpecialText = null;
+    private Image m_HealthPointBar = null;
+    private Image m_SpecialPointBar = null;
+    private TestGroupMemberData m_MemberData = null;
+
+    public static InventoryGroupMemberButton prefab
+    {
+        get
+        {
+            if(m_Prefab == null)
+            {
+                m_Prefab = Resources.Load<InventoryGroupMemberButton>("Prefabs/Button/GroupMemberButton");
+            }
+            return m_Prefab;
+        }
+    }
+
+    public Image playerImage
+    {
+        get
+        {
+            if(m_PlayerImage == null)
+            {
+                m_PlayerImage = transform.GetChild(1).gameObject.GetComponent<Image>();
+            }
+            return m_PlayerImage;
+        }
+        set
+        {
+            m_PlayerImage = value;
+        }
+    }
+
+    public new Image selectedImage
+    {
+        get
+        {
+            if (m_SelectedImage == null)
+            {
+                m_SelectedImage = transform.GetChild(0).gameObject.GetComponent<Image>();
+            }
+            return m_SelectedImage;
+        }
+    }
+
+    public TestGroupMemberData testMemberData
+    {
+        get
+        {
+            return m_MemberData;
+        }
+        set
+        {
+            m_MemberData = value;
+
+            m_HealthText.text = m_MemberData.m_Health + "/" + m_MemberData.m_BaseHealth;
+            Vector3 l_HealthBarScale = m_HealthPointBar.transform.localScale;
+            l_HealthBarScale.x = m_MemberData.m_Health / m_MemberData.m_BaseHealth;
+            m_HealthPointBar.transform.localScale = l_HealthBarScale;
+
+            m_SpecialText.text = m_MemberData.m_SpecialPoints + "/" + m_MemberData.m_BaseSpecialPoints;
+            Vector3 l_SpecialBarScale = m_SpecialPointBar.transform.localScale;
+            l_SpecialBarScale.x = m_MemberData.m_SpecialPoints / m_MemberData.m_BaseSpecialPoints;
+            m_SpecialPointBar.transform.localScale = l_SpecialBarScale;
+        }
+    }
+
+    public override void Awake()
+    {
+        m_PlayerImage = playerImage;
+        m_SelectedImage = selectedImage;
+
+        playerImage.gameObject.SetActive(true);
+        selectedImage.gameObject.SetActive(false);
+
+        GameObject l_PlayerStatInfo = transform.FindChild("Info").gameObject;
+
+        m_HealthText = l_PlayerStatInfo.transform.FindChild("HealthBarText").GetComponent<Text>();
+        m_SpecialText = l_PlayerStatInfo.transform.FindChild("SpecialBarText").GetComponent<Text>();
+        m_HealthPointBar = l_PlayerStatInfo.transform.FindChild("HealthBarBackground").GetComponent<Image>();
+        m_SpecialPointBar = l_PlayerStatInfo.transform.FindChild("SpecialBarBackground").GetComponent<Image>();
+    }
+
+    public override void Select(bool p_Value)
+    {
+        m_Selected = p_Value;
+        selectedImage.gameObject.SetActive(m_Selected);
+    }
+}
--- Assets/Codes/JourneySystemClasses/InventoryClasses/Views/InventoryEquipmentView.cs
+++ Assets/Codes/JourneySystemClasses/InventoryClasses/Views/InventoryEquipmentView.cs
@@ -1,12 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class InventoryEquipmentView : InventoryView
 {
+    #region Variables
+
     private ButtonList m_SlotButtonList;
     private ButtonList m_ItemButtonList;
+    private ButtonList m_GroupButtonList;
     private Text m_DescriptionText;
+    private GameObject m_PlayerStat;
+    private Text m_HealthStatText;
+    private Text m_SpecialPointStatText;
+    private Text m_AttackStatText;
+    private Text m_DefenseStatText;
+    private Text m_SpeedStatText;
+    private int m_EmptySlotsCount = 4;
+
+    // test data
+    List<TestGroupMemberData> m_TestGroupMemberDataList = null;
+
+    #endregion
+
+    #region Properties
 
     public ButtonList slotButtonList
     {
@@ -32,6 +50,18 @@ public class InventoryEquipmentView : InventoryView
         }
     }
 
+    public ButtonList groupButtonList
+    {
+        get
+        {
+            if(m_GroupButtonList == null)
+            {
+                m_GroupButtonList = parent.transform.FindChild("GroupList").GetComponent<ButtonList>();
+            }
+            return m_GroupButtonList;
+        }
+    }
+
     private Text descriptionText
     {
         get
@@ -44,32 +74,124 @@ public class InventoryEquipmentView : InventoryView
         }
     }
 
+    private GameObject playerStat
+    {
+        get
+        {
+            if(m_PlayerStat == null)
+            {
+                m_PlayerStat = parent.transform.FindChild("PlayerStat").gameObject;
+                m_HealthStatText = m_PlayerStat.transform.FindChild("HealthStat").GetComponent<Text>();
+                m_SpecialPointStatText = m_PlayerStat.transform.FindChild("SpecialPointsStat").GetComponent<Text>();
+                m_AttackStatText = m_PlayerStat.transform.FindChild("AttackStat").GetComponent<Text>();
+                m_DefenseStatText = m_PlayerStat.transform.FindChild("DefenseStat").GetComponent<Text>();
+                m_SpeedStatText = m_PlayerStat.transform.FindChild("SpeedStat").GetComponent<Text>();
+            }
+            return m_PlayerStat;
+        }
+    }
+
+    #endregion
+
+    #region Methods
+
     public InventoryEquipmentView(InventoryPanel p_Parent)
     {
         parent = p_Parent;
+        InitGroupData();
+        InitGroupButtonList();
+        InitEmptySlots();
     }
 
     public override void Init()
     {
-        m_SlotButtonList = slotButtonList;
-        m_SlotButtonList.AddCancelAction(CancelAction);
-        m_SlotButtonList.AddKeyArrowAction(InitItemsList);
-        m_SlotButtonList.isActive = false;
-        InitSlots();
+        InitSlotButtonList();
+        InitItemButtonList();
+        m_DescriptionText = descriptionText;
+    }
+
+    private void InitGroupData()
+    {
+        m_TestGroupMemberDataList = new List<TestGroupMemberData>();
+        TestGroupMemberData l_GroupMemberData = new TestGroupMemberData(10.0f, 10.0f, 5.0f, 20.0f, 5.0f, 5.0f, 1.0f);
+        m_TestGroupMemberDataList.Add(l_GroupMemberData);
+
+        l_GroupMemberData = new TestGroupMemberData(85.0f, 100.0f, 77.0f, 100.0f, 23.0f, 17.0f, 3.3f);
+        m_TestGroupMemberDataList.Add(l_GroupMemberData);
+    }
+
+    private void InitGroupButtonList()
+    {
+        m_GroupButtonList = groupButtonList;
+        m_GroupButtonList.AddCancelAction(CancelAction);
+        m_GroupButtonList.AddKeyArrowAction(ShowGroupMemberInfo);
+        m_GroupButtonList.isActive = false;
+
+        foreach(var groupMemberData in m_TestGroupMemberDataList)
+        {
+            AddGroupMember(groupMemberData);
+        }
+    }
+
+    private void AddGroupMember(TestGroupMemberData groupMemberData)
+    {
+        InventoryGroupMemberButton l_Button = UnityEngine.Object.Instantiate(InventoryGroupMemberButton.prefab);
+        l_Button.testMemberData = groupMemberData;
+        l_Button.AddAction(SelectSlotList);
+        m_GroupButtonList.AddButton(l_Button);
+    }
+
+    private void SelectSlotList()
+    {
+        m_GroupButtonList.isActive = false;
+        m_SlotButtonList.isActive = true;
+    }
+
+    private void InitItemButtonList()
+    {
         m_ItemButtonList = itemButtonList;
         m_ItemButtonList.AddCancelAction(DeselectItemList);
         m_ItemButtonList.AddKeyArrowAction(ShowDescription);
         m_ItemButtonList.isActive = false;
-        InitItemsList();
-        m_DescriptionText = descriptionText;
+    }
+
+    private void InitSlotButtonList()
+    {
+        m_SlotButtonList = slotButtonList;
+        m_SlotButtonList.AddCancelAction(DeselectSlotList);
+        m_SlotButtonList.AddKeyArrowAction(InitItemsList);
+        m_SlotButtonList.isActive = false;
+    }
+
+    private void DeselectSlotList()
+    {
+        m_GroupButtonList.isActive = true;
+        m_SlotButtonList.isActive = false;
     }
 
     public override void Disable()
     {
-        m_SlotButtonList.RemoveCancelAction(CancelAction);
+        m_SlotButtonList.RemoveCancelAction(DeselectSlotList);
         m_SlotButtonList.RemoveKeyArrowAction(InitItemsList);
         m_ItemButtonList.RemoveCancelAction(DeselectItemList);
         m_ItemButtonList.RemoveKeyArrowAction(ShowDescription);
+    }
+
+    private void ShowGroupMemberInfo()
+    {
+        InitSlots(PlayerInventory.GetInstance().GetInventorySlotData());
+        InventoryGroupMemberButton l_GroupMemberButton = (InventoryGroupMemberButton) m_GroupButtonList[m_GroupButtonList.currentButtonId];
+        playerStat.gameObject.SetActive(true);
+        m_HealthStatText.text = "HP : " + l_GroupMemberButton.testMemberData.m_Health;
+        m_SpecialPointStatText.text = "SP : " + l_GroupMemberButton.testMemberData.m_SpecialPoints;
+        m_AttackStatText.text = "Atk : " + l_GroupMemberButton.testMemberData.m_AttackStat;
+        m_DefenseStatText.text = "Def : " + l_GroupMemberButton.testMemberData.m_DefenseStat;
+        m_SpeedStatText.text = "Speed : " + l_GroupMemberButton.testMemberData.m_SpeedStat; 
+    }
+
+    private void ClearGroupMemberInfo()
+    {
+        playerStat.gameObject.SetActive(false);
     }
 
     // TODO : Сделать нормальное описание предметов
@@ -89,18 +211,33 @@ public class InventoryEquipmentView : InventoryView
         m_DescriptionText.text = "";
     }
 
-    private void InitSlots()
+    /// <summary>
+    /// Создание пустого списка слотов
+    /// </summary>
+    private void InitEmptySlots()
     {
-        Dictionary<string, InventorySlotData> lSlotData = PlayerInventory.GetInstance().GetInventorySlotData();
-        foreach (var lKey in lSlotData.Keys)
+        slotButtonList.Clear();
+        for (int i = 0; i < m_EmptySlotsCount; i++)
         {
-            AddSlot(lSlotData[lKey]);
+            InventorySlotButton l_Button = UnityEngine.Object.Instantiate(InventorySlotButton.prefab);
+            l_Button.title = "-------";
+            slotButtonList.AddButton(l_Button);
+        }
+        slotButtonList.isActive = false;
+    }
+
+    private void InitSlots(Dictionary<string, InventorySlotData> p_SlotData)
+    {
+        slotButtonList.Clear();
+        foreach (var lKey in p_SlotData.Keys)
+        {
+            AddSlot(p_SlotData[lKey]);
         }
     }
 
     public void AddSlot(InventorySlotData pSlotData)
     {
-        InventorySlotButton l_Button = Object.Instantiate(InventorySlotButton.prefab);
+        InventorySlotButton l_Button = UnityEngine.Object.Instantiate(InventorySlotButton.prefab);
 
         if (PlayerInventory.GetInstance().GetItemCount(pSlotData.itemId) == 0)
             pSlotData.itemId = string.Empty;
@@ -128,7 +265,7 @@ public class InventoryEquipmentView : InventoryView
 
     public override void AddItem(InventoryItemData pInventoryItemData)
     {
-        InventoryEquipmentButton l_Button = Object.Instantiate(InventoryEquipmentButton.prefab);
+        InventoryEquipmentButton l_Button = UnityEngine.Object.Instantiate(InventoryEquipmentButton.prefab);
         l_Button.title = LocalizationDataBase.GetInstance().GetText("Item:" + pInventoryItemData.id);
         l_Button.itemId = pInventoryItemData.id;
         l_Button.AddAction(SelectItem);
@@ -156,13 +293,16 @@ public class InventoryEquipmentView : InventoryView
 
     public override void CancelAction()
     {
-        m_SlotButtonList.isActive = false;
+        m_GroupButtonList.isActive = false;
         parent.tabButtonList.isActive = true;
+        InitEmptySlots();
+        ClearGroupMemberInfo();
     }
 
     public override void Confrim()
     {
-        m_SlotButtonList.isActive = true;
+        m_GroupButtonList.isActive = true;
+        ShowGroupMemberInfo();
     }
 
     public override void SelectItem()
@@ -207,5 +347,8 @@ public class InventoryEquipmentView : InventoryView
     {
         slotButtonList.UpdateKey();
         itemButtonList.UpdateKey();
+        groupButtonList.UpdateKey();
     }
+
+    #endregion
 }
