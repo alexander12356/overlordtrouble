65ffdf7abd3f9903712b401d247158dd2c1baa3c
Alexander12356 <alexander12356@rambler.ru>
2017-02-05 19:34:30 +0900

    Баг с ивентами https://app.hacknplan.com/p/15097/board?categoryId=1&milestoneId=23280&taskId=170&tabId=1 fix

---
+++ Assets/Codes/JourneySystemClasses/ActionsClasses/ChangeActorDirectionAction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeActorDirectionAction : MonoBehaviour
+{
+    [SerializeField]
+    private ActorDirection m_NewDirection = ActorDirection.NONE;
+
+    [SerializeField]
+    private JourneyActor m_TargetActor = null;
+
+    public void Run()
+    {
+        m_TargetActor.SetDirection(m_NewDirection);
+    }
+}
