c5b1f36145cae579ca32855f81f913e64ed14b63
Alexander12356 <alexander12356@rambler.ru>
2017-03-18 20:52:33 +0900

    Реализовать шаг катсцены для забеления и разбеления экрана https://app.hacknplan.com/p/15097/board?categoryId=1&milestoneId=23280&taskId=225&tabId=1 fix Добавить новый шаг катсцены(превращение лешего) в истинной концовке https://app.hacknplan.com/p/15097/board?categoryId=1&milestoneId=23280&taskId=226&tabId=1 fix

--- Assets/Codes/GUIClasses/ScreenFader.cs
+++ Assets/Codes/GUIClasses/ScreenFader.cs
@@ -1,37 +1,51 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
 
-public class ScreenFader : MonoBehaviour {
+using UnityEngine;
+using UnityEngine.UI;
 
-    Animator anim;
-    bool isFading = false;
+public class ScreenFader : MonoBehaviour
+{
+    private Animator m_Animator = null;
+    private Image m_Image = null;
+
+    private bool m_IsFading = false;
 	
-    // Use this for initialization
-	void Start () {
-        anim = GetComponent<Animator>();
-	}
+	private void Awake ()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Image = GetComponent<Image>();
+    }
 
     public IEnumerator FadeToClear()
     {
-        isFading = true;
-        anim.SetTrigger("FadeIn");
+        m_IsFading = true;
+        m_Animator.SetTrigger("FadeIn");
 
-        while (isFading)
+        while (m_IsFading)
+        {
             yield return null;
+        }
     }
 
     public IEnumerator FadeToBlack()
     {
-        isFading = true;
-        anim.SetTrigger("FadeOut");
+        m_IsFading = true;
+        m_Animator.SetTrigger("FadeOut");
 
-        while (isFading)
+        while (m_IsFading)
+        {
             yield return null;
+        }
     }
 
     // Called by Animation
-    void AnimationComplete()
+    private void AnimationComplete()
     {
-        isFading = false;
+        m_IsFading = false;
+    }
+
+    public void ChangeColor(Color p_Color)
+    {
+        m_Image.color = p_Color;
     }
 }
---
+++ Assets/Codes/JourneySystemClasses/CutsceneClasses/Steps/ScreenFadeStep.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenFadeStep : BaseStep
+{
+    [SerializeField]
+    private ScreenFader m_ScreenFader = null;
+
+    [SerializeField]
+    private bool m_ToBlack = false;
+
+    [SerializeField]
+    private Color m_ScreenColor = Color.black;
+
+    public override void StartStep()
+    {
+        base.StartStep();
+
+        StartCoroutine(FadeScreen());
+    }
+
+    private IEnumerator FadeScreen()
+    {
+        if (m_ToBlack)
+        {
+            yield return m_ScreenFader.FadeToBlack();
+        }
+        else
+        {
+            yield return m_ScreenFader.FadeToClear();
+        }
+
+        EndStep();
+    }
+}
