18d97b898db3892cab4217978467731fcb272880
Alexander12356 <alexander12356@rambler.ru>
2017-03-22 21:29:06 +0900

    Первый бой с курой https://app.hacknplan.com/p/15097/board?categoryId=1&milestoneId=23280&taskId=241&tabId=1 fix

--- Assets/Codes/BattleSystemClasses/BattleSystemTutorial.cs
+++ Assets/Codes/BattleSystemClasses/BattleSystemTutorial.cs
@@ -4,12 +4,14 @@ public class BattleSystemTutorial : BattleSystemMobs
 {
     public override void Lose()
     {
+        m_IsLose = true;
         SetVisibleAvatarPanel(false);
 
         TextPanel l_TextPanel = Instantiate(TextPanel.prefab);
-        l_TextPanel.SetText(new List<string>() { LocalizationDataBase.GetInstance().GetText("GUI:BattleSystem:Lose") });
+        l_TextPanel.SetText(new List<string>() { LocalizationDataBase.GetInstance().GetText("GUI:BattleSystem:Lose", new string[] { BattlePlayer.GetInstance().actorName, BattlePlayer.GetInstance().actorName }) });
         l_TextPanel.AddButtonAction(Retreat);
 
-        ShowPanel(l_TextPanel);
+        BattleShowPanelStep l_Step = new BattleShowPanelStep(l_TextPanel);
+        ResultSystem.GetInstance().AddStep(l_Step);
     }
 }
